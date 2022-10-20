9d7ac8793dfde265a672c3724ac20b99e8a62593
sergiorbotelho <sergiorbotelho@gmail.com>
2022-10-20 17:52:44 -0300

    First commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,120 @@
 import "./css/index.css"
+import Imask from 'imask';
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+
+
+function setcardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DG6F29", "#C69347"],
+    default: ["black", "grey"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0]);
+  ccBgColor02.setAttribute("fill", colors[type][1]);
+  ccLogo.setAttribute("src", `cc-${type}.svg`);
+}
+
+globalThis.setcardType = setcardType;
+
+const securityCode = document.getElementById("security-code");
+const securityCodePattern = {
+  mask: "0000"
+}
+const securityCodeMasked = Imask(securityCode, securityCodePattern);
+
+const expirationDate = document.getElementById("expiration-date");
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: Imask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+    YY: {
+      mask: Imask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
+  }
+}
+const expirationDateMasked = Imask(expirationDate, expirationDatePattern);
+
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000 ",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa"
+    },
+    {
+      mask: "0000 0000 0000 0000 ",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard"
+    },
+    {
+      mask: "0000 0000 0000 0000 ",
+      cardtype: "default"
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+    return foundMask
+  },
+}
+
+const cardNumberMasked = Imask(cardNumber, cardNumberPattern);
+
+const addButton = document.querySelector("#add-card");
+
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado")
+
+});
+
+document.querySelector("form").addEventListener("submit", (event) => event.preventDefault())
+
+const cardHolder = document.querySelector("#card-holder");
+
+cardHolder.addEventListener("input", () => {
+  const ccholder = document.querySelector(".cc-holder .value");
+
+  ccholder.innerText = cardHolder.value.length > 0 ? cardHolder.value : ccholder.innerText = "FULANO DA SILVA"
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value");
+  ccSecurity.innerText = code.length > 0 ? code : "123"
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setcardType(cardType)
+  updateCardNumber(cardNumberMasked.value);
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number");
+  ccNumber.innerText = number.length > 0 ? number : "1234 5678 9012 3456"
+
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value");
+  ccExpiration.innerText = date.length > 0 ? date : "02/32"
+}
---
+++ src/regular.js
@@ -0,0 +1,23 @@
+
+
+//visa
+//inicia com 4  seguido de mais 15 dígitos
+//^4\d{0,15} expressao regular
+4123456789123456
+
+
+
+//master
+
+//inicia com 5, seguido de um digito entre 1 e 5, seguido de mais 2 digitos
+//OU
+//inicia com 22, seuido de um digito entre 2 e 9, seguido de mais 1 digito
+//OU
+//inicia com 2, seguido de um digito entre 3 e 7, seguido de mais 2 digitos
+//seguido de mais 12 digitos
+
+//expressao regular
+//(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+5353535353535353
+2323232323232323
+2234344655455664
